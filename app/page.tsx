0d6f2b4e7263dd86ba60ae8b52e119a57ff27969
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2025-11-14 07:48:13 +0300

    change

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ const CalendarImageWithOverlay = React.memo(function CalendarImageWithOverlay({ 
       {/* Overlay with symmetrical slide animation */}
       <div
         className={`
-          absolute inset-0 bg-gradient-to-r from-purple-900/50 to-purple-900/50
+          absolute inset-0 bg-gradient-to-r from-purple-950/80 to-purple-950/80
           flex flex-col items-center justify-center text-white text-center px-6
           transition-transform duration-700 ease-out
           ${isVisible ? 'translate-x-0' : (isLeft ? '-translate-x-full' : 'translate-x-full')}
